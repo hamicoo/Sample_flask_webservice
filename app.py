4be5fc6dac8d244d622be7ecb82f5e55de08c60a
Hamed Azizi vamerzani <perance.persia@gmail.com>
2018-08-12 14:09:57 +0430

    new

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import  generate_password_hash,check_password_hash
+
+
+
+
 app=Flask(__name__)
-
-
 app.config['SECRET_KEY']='thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////fdb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost/flask'
 db = SQLAlchemy(app)
 
 
@@ -15,18 +17,18 @@ db = SQLAlchemy(app)
 
 
 class user(db.Model):
-    id=db.Column(db.INTEGER,primary_key=True)
-    public_id=db.Column(db.String(10),unique=True)
+    id=db.Column(db.INT,primary_key=True)
+    public_id=db.Column(db.String(50),unique=True)
     name=db.Column(db.String(50))
-    password=db.Column(db.String(80))
+    password=db.Column(db.TEXT)
     admin=db.Column(db.Boolean)
 
 
 class todo(db.Model):
-    id=db.Column(db.INTEGER,primary_key=True)
+    id=db.Column(db.INT,primary_key=True)
     text=db.Column(db.String(60))
     complete = db.Column(db.Boolean)
-    user_id=db.Column(db.INTEGER)
+    user_id=db.Column(db.INT)
 
 
 
@@ -46,7 +48,7 @@ def get_one_user():
 def create_user():
     data=request.get_json()
     hashed_password=generate_password_hash(data['password'], method='sha256')
-    new_user=user(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password,admin=False)
+    new_user= user(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password,admin=False)
     db.session.add(new_user)
     db.session.commit()
     users = user.query.all()
@@ -68,5 +70,7 @@ def delete_user():
 
 if __name__=='__main__':
     app.run(debug=True)
+    db.create_all()
+    db.session.commit()
 
 
